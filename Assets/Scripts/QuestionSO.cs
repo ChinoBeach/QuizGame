a50c086dffd1a674bf10806ab706d7b8ecd2c5d3
ChinoBeach <beach988@morrisville.edu>
2022-03-11 15:27:45 -0500

    arrays

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,10 +7,23 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)] 
     [SerializeField] string strQuestion = "Enter New Question Text Here";
+    [SerializeField] string[] strArrayAnswers = new string[4];
+
+    [SerializeField] int intCorrectIndex; 
+
 
     public string GetQuestion()
     {
         return strQuestion; 
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return intCorrectIndex;
+    }
+    public string GetAnswer(int index)
+    {
+        return strArrayAnswers[index];
     }
 
 }
